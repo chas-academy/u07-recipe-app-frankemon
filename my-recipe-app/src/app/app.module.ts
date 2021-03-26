dba2adc6cf81f2628e08e4acd847e554ed634828
frankemon <70698241+frankemon@users.noreply.github.com>
2021-03-26 17:54:53 +0100

    add tailwind cdn, header, footer, recipe component

--- my-recipe-app/src/app/app.module.ts
+++ my-recipe-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
