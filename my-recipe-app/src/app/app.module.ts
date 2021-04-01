48af786332145ae3501ddd5ea0df6be4cb0cf711
frankemon <70698241+frankemon@users.noreply.github.com>
2021-04-01 16:39:00 +0200

    add home coponent, routing, redo structure

--- my-recipe-app/src/app/app.module.ts
+++ my-recipe-app/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 import { RecipesComponent } from './recipes/recipes.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: "full"}
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +21,8 @@ import { RecipesComponent } from './recipes/recipes.component';
     RecipesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
