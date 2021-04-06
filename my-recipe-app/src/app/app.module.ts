6376050378dcc51010e4cc8413df1b08856fc5bc
frankemon <frankie.hugo@hotmail.com>
2021-04-06 18:47:50 +0200

    add httpclient, error handling, get recipe from mock server

--- my-recipe-app/src/app/app.module.ts
+++ my-recipe-app/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,6 +29,9 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-recipe-app/src/app/recipe.service.ts
+++ my-recipe-app/src/app/recipe.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Observable, of } from 'rxjs';
 
@@ -7,22 +9,49 @@ import { RECIPES } from './mock-recipes';
 import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class RecipeService {
+  private recipesUrl = 'api/recipes'; // URL to web api
 
-  constructor(private messageService: MessageService) { }
-
-   getRecipes(): Observable<Recipe[]> {
-    const recipes = of(RECIPES);
-    this.messageService.add('RecipeService: fetched ecipe');
-    return recipes;
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+  /** GET recipes from the "server" */
+  getRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(this.recipesUrl).pipe(
+      tap((_) => this.log('fetched recipes')),
+      catchError(this.handleError<Recipe[]>('getRecipes', []))
+    );
+  }
+  getRecipe(id: number): Observable<Recipe> {
+    const url = `${this.recipesUrl}/${id}`;
+    return this.http.get<Recipe>(url).pipe(
+      tap((_) => this.log(`fetched recipe id=${id}`)),
+      catchError(this.handleError<Recipe>(`getRecipe id=${id}`))
+    );
+  }
+  private log(message: string) {
+    this.messageService.add(`RecipeService: ${message}`);
   }
 
-  getRecipe(id: number): Observable<Recipe> {
-    const recipe = RECIPES.find(h => h.id === id) as Recipe;
-    this.messageService.add(`RecipeService: fetched recipe id=${id}`);
-    return of(recipe);
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
