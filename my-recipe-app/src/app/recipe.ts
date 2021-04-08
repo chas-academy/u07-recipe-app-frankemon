9ef4a3d472aa631ee82f9a729bb6def5be00a0c3
frankemon <frankie.hugo@hotmail.com>
2021-04-08 19:20:40 +0200

    add fetch data from api, print data to recipe-details, ingredients ngFor

--- my-recipe-app/src/app/recipe.ts
+++ my-recipe-app/src/app/recipe.ts
@@ -1,4 +1,15 @@
 export interface Recipe {
   id: number;
   title: string;
+  image: string;
+  summary: string;
+  servings: number;
+  readyInMinutes: number;
+  extendedIngredients: object;
+  vegetarian: boolean;
+  vegan: boolean;
+  glutenFree: boolean;
+  dairyFree: boolean;
+  dishTypes: string[];
+  instructions: string;
 }
