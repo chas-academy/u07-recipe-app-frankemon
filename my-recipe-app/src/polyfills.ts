50db7775f26f06ba29ba29e64f329a5f13526104
frankemon <frankie.hugo@hotmail.com>
2021-08-23 23:29:10 +0200

    edit polyfills

--- my-recipe-app/src/polyfills.ts
+++ my-recipe-app/src/polyfills.ts
@@ -54,11 +54,14 @@
  *
  */
 
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
